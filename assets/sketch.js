afdce0fae9d6d4698211efad99f5e07b2a998caf
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-03-31 22:33:57 +0300

    update start function

--- assets/sketch.js
+++ assets/sketch.js
@@ -24,7 +24,7 @@ function createText() {
   winTextPlayer2.style('display', 'none');
   winTextPlayer2.style('color', 'red');
   winTextPlayer2.position(width / 2 - 150, 100);
-  instructionText = createP("Press space to Start, press up or down/w or s arrow to move");
+  instructionText = createP("Press b to Start, press up or down/w or s arrow to move");
   instructionText.style('display', 'none');
   instructionText.style('color', 'red');
   instructionText.position(width / 5, 100);
@@ -116,6 +116,8 @@ function changeVel() {
   } else if (keyIsDown(83)) {
     paddle1.setVelocity(height/100);
     paddle2.setVelocity(0);
+  } else if (key == " ") {
+    ball.pause = false;
   } else {
     paddle1.setVelocity(0);
     paddle2.setVelocity(0);
